5f4207a33b8b234539aecf7132c1cbc0ee8c10a9
geroxus <goldto@web.de>
2020-11-25 19:22:51 +0100

    added colour coding for output and removed lots of useless output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from colorama import Fore
 
 data: dict = {}
 final: dict = {}
@@ -35,7 +36,7 @@ all_dates = pd.DataFrame.from_dict(data, orient='index', columns=['Date'])
 with pd.ExcelWriter('./all_dates.xlsx') as writer:
     all_dates.to_excel(writer)
 
-all_dates.info()  # print out info about the data frame so the user knows whether it was successful or not
+# all_dates.info()  # print out info about the data frame so the user knows whether it was successful or not
 
 # excel_sheet: str = 'K:/Uni/stundenplan.xlsx'  # path to excel sheet to compare all_dates against
 # print('start reading \"' + excel_sheet + '\"')
@@ -44,25 +45,29 @@ all_dates.info()  # print out info about the data frame so the user knows whethe
 # after we are done
 # filter_df = pd.read_excel(io=excel_sheet, usecols='H:J', nrows=6, header=0)
 filter_df = pd.read_clipboard(sep=None, index_col=None, header=None, names=['Klausur', 'a'])
-print(filter_df.to_string())
+# print(filter_df.to_string())
 filter_list: list = filter_df.get('Klausur').to_list()
 for index, entry in enumerate(filter_list):
-    if '/' in entry:
-        filter_list[index] = entry.split('/')[0]
+    if ' /' in entry:
+        filter_list[index] = entry.split(' /')[0]
         # print(filter_list[index])
 # print(filter_list)
 print('Anzahl zu findender Termine:', len(filter_list))
 # this is probably a really good example to use list comprehension at but it's dicts and I can't be bothered |future me:
 #                                                                                                            |     lmao
 for key_value in filter_list:
+    found: bool = False
     for key in data.keys():
-        if key_value in key:
-            print(key + ': ' + data[key])
+        if key_value == key:
+            print(Fore.BLUE + key + ': ' + data[key] + Fore.RESET)
             final[key] = data[key]
+            found = True
             break
+    if not found:
+        print(Fore.RED + 'Klausur \"' + key_value + '\" konnte nicht gefunden werden.' + Fore.RESET)
 print('Termine gefunden')
 out = pd.DataFrame.from_dict(final, orient='index', columns=['Date'])
-print(out.to_string())
+# print(out.to_string())
 with pd.ExcelWriter('./exam_dates.xlsx', mode='w') as writer:
     out.to_excel(writer, header=False, index=False)
 
